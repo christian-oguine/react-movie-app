156d0ae26a0a3b1608c56414371a43dae75c0ee9
Oguine Christian <christian.oguine@student.howest.be>
2026-02-10 10:37:47 +0100

    refactor App component layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="bg-black min-h-screen">
+      <div className="flex items-center justify-between bg-gray-800 p-4">
+        <h1 className="font-bold text-white ">Logo</h1>
+
+        <div className="flex gap-4 ml-3">
+            <span className="text-gray-400">Home</span>
+            <span className="text-gray-400">Movies</span>
+            <span className="text-gray-400">Trending</span>
+            <span className="text-gray-400">My List</span>
+        </div>
+      </div>
+
     </div>
   )
 }
