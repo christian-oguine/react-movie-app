8b515fd7dbe9e87a297644f71ead3b2eea94b22a
Oguine Christian <christian.oguine@student.howest.be>
2026-02-10 16:16:28 +0100

    add movie data and enhance App component with search functionality and styling

---
+++ src/data/movies.js
@@ -0,0 +1,68 @@
+export const movies = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        rating: 9.3,
+        poster: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg"
+    },
+    {
+        id: 2,
+        title: "The Godfather",
+        rating: 9.2,
+        poster: "https://m.media-amazon.com/images/I/41+eK8zBwQL._AC_.jpg"
+    },  
+    {
+        id: 3,
+        title: "The Dark Knight",
+        rating: 9.0,
+        poster: "https://m.media-amazon.com/images/I/51EbJjlLJGL._AC_.jpg"
+    },
+    {
+        id: 4,
+        title: "Pulp Fiction",
+        rating: 8.9,
+        poster: "https://m.media-amazon.com/images/I/51V5ZpFyaFL._AC_.jpg"
+    },
+    {
+        id: 5,
+        title: "Forrest Gump",
+        rating: 8.8,
+        poster: "https://m.media-amazon.com/images/I/41c9r0YlHFL._AC_.jpg"
+    },
+    {
+        id: 6,
+        title: "Inception",
+        rating: 8.7,
+        poster: "https://m.media-amazon.com/images/I/51s+Jr6ZyKL._AC_.jpg"
+    },  
+    {
+        id: 7,
+        title: "Fight Club",
+        rating: 8.8,
+        poster: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg"
+    },
+    {
+        id: 8,
+        title: "The Matrix",
+        rating: 8.7,
+        poster: "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg"
+    },
+    {
+        id: 9,
+        title: "Goodfellas",
+        rating: 8.7,
+        poster: "https://m.media-amazon.com/images/I/51oBxmV-dML._AC_.jpg"
+    },
+    {
+        id: 10,
+        title: "The Lord of the Rings: The Return of the King",
+        rating: 8.9,
+        poster: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg"
+    },
+    {
+        id: 11,
+        title: "Playing with Fire",
+        rating: 8.8,
+        poster: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg"
+    }
+]
